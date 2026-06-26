c1b296e25cb60dd446cdbfe9ce24789d7313bc21
franciscolinoneto-cmyk <francisco.lino.neto@escola.pr.gov.br>
2026-06-26 17:29:17 -0300

    vbn

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,36 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Game of Thrones',
+    'Quem foi Rhaegar Targaryen?',
+    'Príncipe herdeiro da Casa Targaryen, filho do Rei Louco e considerado um homem culto, habilidoso e respeitado. Seu relacionamento com Lyanna Stark desencadeou a Rebelião de Robert Baratheon, conflito que levou ao fim da dinastia Targaryen. Além disso, ele era o verdadeiro pai de Jon Snow, um dos maiores segredos da história de Westeros'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'House of the Dragon',
+    'Quem foi Rhaenyra Targaryen?',
+    'Filha mais velha do rei Viserys I Targaryen e a primeira mulher nomeada oficialmente como herdeira do Trono de Ferro. Sua reivindicação ao trono foi contestada após a morte do pai, dando início à Dança dos Dragões, uma guerra civil que dividiu a Casa Targaryen e levou à morte de dragões e de grande parte da família real'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Game of Thrones',
+    'Quem foi Daenerys Targaryen?',
+    'Filha do rei Aerys II Targaryen, o "Rei Louco", e uma das últimas sobreviventes da Casa Targaryen após a Rebelião de Robert. Criada no exílio em Essos, tornou-se conhecida como a "Mãe dos Dragões" ao trazer três dragões de volta ao mundo e liderar uma campanha para reconquistar o Trono de Ferro'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'House of the Dragon',
+    '',
+    ''
+)
+
+criaCartao(
+    'Game of Thrones',
+    'Quem foi Oberyn Martell?',
+    'Príncipe de Dorne e membro da Casa Martell, conhecido como “A Víbora Vermelha”. Era famoso por sua habilidade com venenos e combate, além de buscar vingança pela morte de sua irmã Elia Martell, assassinada durante a queda dos Targaryen. Morreu em Porto Real durante um julgamento por combate contra Gregor Clegane'
+)
+
+criaCartao(
+    'House of the Dragon',
+    '',
+    ''
 )
 
